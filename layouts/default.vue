638f504308318aedce6b8a3d1f70a182088f19e6
Siguretuki <siguretukikohane@gmail.com>
2023-09-21 00:15:41 +0900

    default.vue　作成

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    layouts/default.vue
+    <nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'DefaultLayout'
+})
+</script>
